4ad8bba8cef26dd3c323539a79e16534ab30f1a0
Maik Michel <maik.michel78@gmail.com>
2022-08-19 15:43:20 +0200

    Swap scan with define

--- build.sh
+++ build.sh
@@ -550,6 +550,7 @@ function write_install_schemas(){
         echo "Prompt ..                       Version: $mode $version " >> "$target_install_file"
         echo "Prompt .............................................................................. " >> "$target_install_file"
         echo "set serveroutput on" >> "$target_install_file"
+        echo "set define off" >> "$target_install_file"
         echo "" >> "$target_install_file"
 
         if [[ "${mode}" == "patch" ]]; then
@@ -574,7 +575,7 @@ function write_install_schemas(){
 
             # set scan to on, to make use of vars inside main schema-hooks
             if [[ "${path}" == ".hooks/pre" ]] || [[ "${path}" == ".hooks/post" ]]; then
-              echo "set scan on" >> "$target_install_file"
+              echo "set define on" >> "$target_install_file"
             fi
 
             # pre folder-hooks (something like db/schema/.hooks/pre/tables)
@@ -664,7 +665,9 @@ function write_install_schemas(){
 
              # set scan to off, to make use of vars inside main schema-hooks
             if [[ "${path}" == ".hooks/pre" ]] || [[ "${path}" == ".hooks/post" ]]; then
-              echo "set scan off" >> "$target_install_file"
+              echo "set define off" >> "$target_install_file"
+            else
+
             fi
 
             echo "Prompt" >> "$target_install_file"
