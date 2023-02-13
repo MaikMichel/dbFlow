ee0599ac16701ecf8176ae89df7dfa25b5166c6d
Maik Michel <maik.michel78@gmail.com>
2023-02-13 21:42:35 +0100

    Set default option to remove existing workspace with same name and add a warning message after setup processed

--- scripts/setup/workspaces/workspace/create_00_workspace.sql
+++ scripts/setup/workspaces/workspace/create_00_workspace.sql
@@ -21,10 +21,16 @@ begin
    where workspace = upper('^workspace');
 
   dbms_output.put_line('Workspace ^workspace allready installed!');
+
+  apex_instance_admin.remove_workspace(p_workspace         => upper('^workspace'),
+                                       p_drop_users        => 'Y',
+                                       p_drop_tablespaces  => 'Y' );
+
+  raise no_data_found;
 exception
   when no_data_found then
   -- workspace does not exist, so add it
-    apex_instance_admin.add_workspace (p_workspace => '^workspace',
+    apex_instance_admin.add_workspace (p_workspace      => upper('^workspace'),
                                        p_primary_schema => upper('^app_schema'));
 
     commit;
